3675a6fa24903164a7204db86cd3fa3ab1f1293e
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-27 19:02:39 -0500

    Update create_vm.sh

--- proxmox/create_vm.sh
+++ proxmox/create_vm.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# https://pve.proxmox.com/wiki/Windows_10_guest_best_practices
+
+sudo wget -O /mnt/nas/data1/iso/template/iso/virtio-win.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
+
+
 # Create the Proxmox VM
 # [10.12. Managing Virtual Machines with qm](https://pve.proxmox.com/pve-docs/pve-admin-guide.html#_managing_virtual_machines_with_span_class_monospaced_qm_span)
 
@@ -14,12 +19,14 @@ qm create $VMID \
   --memory 4096 \
   --ostype win10 \
   --ide2 nas-data1-iso:iso/Win10_2004_English_x64.iso,media=cdrom \
+  --ide3 nas-data1-iso:iso/virtio-win.iso,media=cdrom \
   --scsi0 nas-data1-vm:1,format=qcow2,discard=on,ssd=1 \
   --scsihw virtio-scsi-pci \
   --bootdisk scsi0 \
   --net0 virtio,bridge=vmbr0,firewall=1 \
   --onboot 1 \
-  --numa 0
+  --numa 0 \
+  --agent 1
 
 qm resize $VMID scsi0 256G # [resize disks](https://pve.proxmox.com/wiki/Resize_disks)
 
